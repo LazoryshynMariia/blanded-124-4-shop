2bdbb098bc76f499fe8798c5f5ea6b4c1027fbff
Mariia <ledimoon21@gmail.com>
2026-02-22 01:23:21 +0200

    Task 1-4

---
+++ src/js/products-api.js
@@ -0,0 +1,21 @@
+import axios from 'axios';
+
+axios.defaults.baseURL = 'https://dummyjson.com/products';
+
+export const getCatalog = async () => {
+  const { data } = await axios.get('/category-list');
+  const res = ['All', ...data];
+  return res;
+};
+
+export const getProducts = async (currentPage = 1) => {
+  const url = `?limit=12&skip=${(currentPage - 1) * 12}`;
+  const { data } = await axios.get(url);
+
+  return data.products;
+};
+export const getProdByCaterogy = async(target, currentPage = 1 ) =>{
+    const {data} = await axios.get(`/category/${target}?limit=12&skip=${(currentPage - 1) * 12}`)
+
+    return data.products
+}
